c0589937a2c13243275506d37f39deb6b7fef210
Nate Hayflick <nhayflick@gmail.com>
2013-04-07 17:11:13 -0700

    adds basic user model and password authentication

---
+++ db/migrate/20130408000246_add_pword_and_confirmation.rb
@@ -0,0 +1,6 @@
+class AddPwordAndConfirmation < ActiveRecord::Migration
+  # def create
+  #   add_column :users, :password, :string
+  #   add_column :users, :password_confirmation, :string
+  # end
+end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,29 +4,49 @@ describe "StaticPages" do
 
   let(:title) { "Ruby on Rails Tutorial Sample App" }
   subject { page }
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector('title', :text => "#{title} | About Us")
+    click_link "Help"
+    page.should have_selector('title', :text => "#{title} | Help")
+    click_link "Contact"
+    page.should have_selector('title', :text => "#{title} | Contact Us")
+    #click_link "Home"
+    #click_link "Sign up now!"
+    #page.should have_selector('title', :text => "#{title} | Sign up")
+    click_link "sample app"
+    page.should have_selector('title', :text => "#{title} | Home")
+  end
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',    text: heading) }
+  end
   
   describe "Home Page" do
 
     before { visit root_path }
-    it { should have_selector('h1', :text => 'Sample App') }
+    let(:heading)    { 'Sample App' }
+
     it { should have_selector('title', :text => "#{title} | Home") }
   end
 
   describe "Help Page" do
     before { visit help_path }
-    it { should have_selector('h1', :text => 'Help') }
+    let(:heading)    { 'Sample App' }
     it { should have_selector('title', :text => "#{title} | Help") }
   end
 
   describe "About Page" do
     before { visit about_path }
-    it { should have_selector('h1', :text => 'About Us') }
+    let(:heading)    { 'Sample App' }
     it { should have_selector('title', :text => "#{title} | About Us") }
   end
 
   describe "Contact Page" do
     before { visit contact_path }
-    it { should have_selector('h1', :text => 'Contact Us') }
+    let(:heading)    { 'Sample App' }
     it { should have_selector('title', :text => "#{title} | Contact Us") }
   end
 end
